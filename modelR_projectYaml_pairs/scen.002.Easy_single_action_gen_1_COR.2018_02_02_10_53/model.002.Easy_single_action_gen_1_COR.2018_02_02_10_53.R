275f9e5a31b75c6413e9fdb7dd8046d58dd0d786
Bill Langford <bill.langford@rmit.edu.au>
2018-06-13 12:24:49 +1000

    Add yaml_file_stored_as var to project.yaml files and re-org dirs
    In all the different old tests to build up the nectar VM to run bdpg,
    it's hard to figure out which version of project.yaml and model.R files
    were used.  So, I'm now saving an appropriately named copy of each
    and adding variables and output in the model.R and project.yaml files
    to echo that information at runtime.  This should make it a little bit
    easier when looking at a run to see what went into it.

--- modelR_projectYaml_pairs/scen.002.Easy_single_action_gen_1_COR.2018_02_02_10_53/model.002.Easy_single_action_gen_1_COR.2018_02_02_10_53.R
+++ modelR_projectYaml_pairs/scen.002.Easy_single_action_gen_1_COR.2018_02_02_10_53/model.002.Easy_single_action_gen_1_COR.2018_02_02_10_53.R
@@ -4,11 +4,14 @@
                         #              model.R              #
                         #-----------------------------------#
 
+#  model.002.Easy_single_action_gen_1_COR.2018_02_02_10_53.R
+
 #===============================================================================
 
 library (bdpgxupaper)
 
-cat ("\n\nIn btzar::model.R()\n\n")
+cat ("\n\nIn btzar::model.R()")
+cat ("\n    Saved as: model.002.Easy_single_action_gen_1_COR.2018_02_02_10_53.R\n\n")
 print (parameters)
 cat ("\nDone with btzar::model.R()\n")
 
