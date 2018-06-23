73fcf8ea9b333ad41981cb39d2e249b2b1f90dce
Bill Langford <bill.langford@rmit.edu.au>
2018-06-23 12:49:53 +1000

    Reinstate very first test setup to restart tzar pollandrun tests on nectar

--- model.R
+++ model.R
@@ -4,18 +4,15 @@
                         #              model.R              #
                         #-----------------------------------#
 
-#  model.002.Easy_single_action_gen_1_COR.2018_02_02_10_53.R
-
 #===============================================================================
 
 library (bdpgxupaper)
 
-cat ("\n\nIn btzar::model.R()")
-cat ("\n    Saved as: model.002.Easy_single_action_gen_1_COR.2018_02_02_10_53.R\n\n")
+cat ("\n\nIn btzar::model.R()\n\n")
 print (parameters)
 cat ("\nDone with btzar::model.R()\n")
 
-xu_paper_main (parameters, emulating_tzar=FALSE)
+#xu_paper_main (parameters, emulating_tzar=FALSE)
 
 #===============================================================================
 
